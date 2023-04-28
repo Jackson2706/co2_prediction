1551c08ed579e305cf52d7fcb4416b4cedcc1a56
Jackson2706 <dung27062002@gmail.com>
2023-04-28 11:21:37 +0700

    init commit

---
+++ co2_prediction.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import tkinter
+import matplotlib
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+matplotlib.use("TkAgg")
+
+def create_recursive_data(data, window_size=5):
+    i = 1
+    while i < window_size:
+        data[f"co2_{i}"] = data["co2"].shift(-i)
+        i = i + 1
+
+    data["target"] = data["co2"].shift(-i)
+    data = data.dropna(axis=0)
+    return data
+
+data = pd.read_csv("co2.csv")
+data["time"] = pd.to_datetime(data["time"])
+data["co2"] = data["co2"].interpolate()
+# fig, ax = plt.subplots()
+# ax.plot(data["time"], data["co2"])
+# ax.set_xlabel("time")
+# ax.set_ylabel("CO2")
+# plt.show()
+
+window_size = 5
+data = create_recursive_data(data=data, window_size=window_size)
+# print(data)
+
+target = "target"
+X = data.drop([target,"time"], axis=1)
+y = data[target]
+train_size = 0.8
+num_samples = len(X)
+x_train = X[:int(num_samples*train_size)]
+y_train  = y[:int(num_samples*train_size)]
+x_test = X[int(num_samples*train_size):]
+y_test = y[int(num_samples*train_size):]
+
+reg = RandomForestRegressor()
+reg.fit(x_train, y_train)
+y_predict = reg.predict(x_test)
+
+print(f'R2: {r2_score(y_true=y_test, y_pred=y_predict)}')
+print(f'MSE: {mean_squared_error(y_true=y_test, y_pred=y_predict)}')
+print(f'MAE: {mean_absolute_error(y_true=y_test, y_pred=y_predict)}')
+
+fig, ax = plt.subplots()
+
+ax.plot(data["time"][:int(num_samples*train_size)], data["co2"][:int(num_samples*train_size)], label = "train")
+ax.plot(data["time"][int(num_samples*train_size):], data["co2"][int(num_samples*train_size):], label = "test")
+ax.plot(data["time"][int(num_samples*train_size):], y_predict, label = "prediction")
+ax.set_xlabel("Time")
+ax.set_ylabel("Co2")
+ax.legend()
+ax.grid()
+plt.show()
